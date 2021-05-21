eda9163eb044f1ffb3d71f4abe6a517b18c41342
AlbinIzmaku <59483738+AlbinIzmaku@users.noreply.github.com>
2021-05-21 18:36:49 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+  <div class="art">
+    <div class="content">
+      <section>
+        <header>Agency</header>
+        <a href="Contact-us.php">About</a>
+        <a href="Contact-us.php">Contact us</a>
+        <a href="Contact-us.php">Team</a>
+      </section>
+      <section>
+        <header>Development</header>
+        <a href="home.php">Website</a>
+        <a href="tour.php">Tour</a>
+        <a href="https://travel.state.gov/">Travel</a>
+      </section>
+      <section>
+        <header>More and more</header>
+        <form action="">
+          <input type="email" placeholder="Your email" id="">
+          <a href="https://www.youtube.com/">Subscribe</a>
+        </form>
+        <div class="social">
+          <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+          <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.telegram.com/"><i class="fab fa-telegram-plane"></i></a>
+        </div>
+      </section>
+    </div>
+    <div class="notice">
+      Copyright protected &copy;
+      <strong>All rights reserved</strong>
+    </div>
+  </div>
+</footer>
