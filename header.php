785b2a22355b53b376da6b40eea64f94080c13d3
AlbinIzmaku <59483738+AlbinIzmaku@users.noreply.github.com>
2021-05-21 18:36:11 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<header>
+  <nav>
+    <div class="navigimi">
+      <img src="images/loggo.png">
+      <ul id="linqet_e_navigimit">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="albania1.php">Albania</a></li>
+        <li><a href="destination.php">Destination</a></li>
+        <li><a href="tour.php">Tours</a></li>
+        <li><a href="Contact-us.php">Contact us</a></li>
+        <div class="button11" id="button-5">
+          <div id="translate"></div>
+          <a href="form.php">Get Ticket</a>
+        </div>
+      </ul>
+      <i id="bar" class="fas fa-bars" aria-hidden="true"></i>
+    </div>
+  </nav>
+</header>
